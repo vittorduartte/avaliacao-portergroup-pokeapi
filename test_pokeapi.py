7f2d96353b157add303dba46beef5703ac9d3947
vittorduartte <mateusriograndense@gmail.com>
2022-05-29 17:29:01 -0300

    Comparando o resultado da requisição com um objeto pokémon esperado

--- test_pokeapi.py
+++ test_pokeapi.py
@@ -1,21 +1,16 @@
 import unittest
 from resources import PokeAPI
+from models import Pokemon
 
 class TestPokeAPI(unittest.TestCase):
     
     def test_mapping_first_pokemon(self):
         api = PokeAPI()
-        response = api.get_all_pokemons(limit=1)
-        
-        expected = {
-            "name": "bulbasaur",
-            "more_details_url": "https://pokeapi.co/api/v2/pokemon/1/",
-            "weight": 69,
-            "abilities": ['overgrow', 'chlorophyll'],
-            "pokemon_type": ['grass', 'poison']
-        }
+        api.build_pokemon_list(limit=1)
+        data = api.get_pokemon_data()
+        expected = Pokemon(name='bulbasaur', more_details_url='https://pokeapi.co/api/v2/pokemon/1/', weight=69, abilities=['overgrow', 'chlorophyll'], pokemon_type=['grass', 'poison'])
 
-        self.assertDictEqual(expected, response["data"][0].__dict__)
+        self.assertEqual(expected.__repr__(), data.__repr__())
 
 if __name__ == '__main__':
     unittest.main()
