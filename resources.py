724090f0f0a319c09e1e0cf5b8fcd84236482a73
vittorduartte <mateusriograndense@gmail.com>
2022-05-29 18:12:28 -0300

    Montando uma lista de objetos e recuperando os dados desta
    Criando um atributo que representa a lista de pokemons obtida na consulta a PokeAPI e implementação do método que resgata estes dados da classe.

--- resources.py
+++ resources.py
@@ -13,8 +13,9 @@ class PokeAPI():
 
     def __init__(self, base_url: str = "https://pokeapi.co/api/v2/pokemon/"):
         self.base_url = base_url
+        self.pokemon_data = []
 
-    def __pokemon_object_mapping(self, pokemon) -> object:
+    def __pokemon_object_mapping(self, pokemon) -> Pokemon:
         '''
         (Private) Método responsável por parsear os dados primários
         e consulta a url individual de mais detalhes, para 
@@ -30,9 +31,9 @@ class PokeAPI():
             response = get(pokemon["url"])
             more_details = loads(response.text)
             abilities = list(
-                map(lambda ability: ability["ability"]["name"], more_details["abilities"]) )
+                map(lambda ability: ability["ability"]["name"], more_details["abilities"]))
             types = list(
-                map(lambda type: type["type"]["name"], more_details["types"]) )
+                map(lambda type: type["type"]["name"], more_details["types"]))
             kwargs = {
                 "name": pokemon["name"],
                 "more_details_url": pokemon["url"],
@@ -45,7 +46,7 @@ class PokeAPI():
         except Exception as e:
             raise e
 
-    def get_all_pokemons(self, offset: int = 0, limit: int = 20) -> dict:
+    def build_pokemon_list(self, offset: int = 0, limit: int = 20) -> None:
         '''
         Método responsável por recuperar os dados primários 
         individuais dos Pokémons via API e os mapeiam para 
@@ -55,14 +56,22 @@ class PokeAPI():
                     offset (int): Deslocamento do intervalo de resultados da consulta
                     limit (int): Limite de resultados na consulta
 
-            Returns:
-                    data (dict): Um dicionário com os dados de Pokémons e informações da requisição.  
         '''
         try:
             response = get(f'{self.base_url}?offset={offset}&limit={limit}')
             object_response = list(
-                map(self.__pokemon_object_mapping, loads(response.text)["results"]) )
+                map(self.__pokemon_object_mapping, loads(response.text)["results"]))
 
-            return {"data": object_response, "message": "Pokémons obtidos com sucesso."}
+            self.pokemon_data = object_response
         except Exception as e:
-            return {"data": False, "message": "Erro ao recuperar os dados de Pokémons.", "error": str(e)}
+            raise e
+
+    def get_pokemon_data(self) -> list:
+        '''
+        Método responsável por retornar a lista de pokémons
+        gerada a partir da consulta feita na PokeAPI.
+
+            Returns:
+                data (list): Lista de objetos Pokémons.
+        '''
+        return [*self.pokemon_data]
