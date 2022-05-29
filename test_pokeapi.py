073ee046c688cc1ec6b0d63175a19e4ad2c73187
vittorduartte <mateusriograndense@gmail.com>
2022-05-29 17:53:34 -0300

    Incluindo testes de retorno de tipo
    Verificação do tipo retornado em uma consulta com mais de um resultado

--- test_pokeapi.py
+++ test_pokeapi.py
@@ -2,15 +2,27 @@ import unittest
 from resources import PokeAPI
 from models import Pokemon
 
+
 class TestPokeAPI(unittest.TestCase):
-    
-    def test_mapping_first_pokemon(self):
+
+    def test_mapping_pokemon_object(self):
         api = PokeAPI()
         api.build_pokemon_list(limit=1)
-        data = api.get_pokemon_data()
-        expected = Pokemon(name='bulbasaur', more_details_url='https://pokeapi.co/api/v2/pokemon/1/', weight=69, abilities=['overgrow', 'chlorophyll'], pokemon_type=['grass', 'poison'])
+        data = api.get_pokemon_data()[0]
+        expected = Pokemon(name='bulbasaur', more_details_url='https://pokeapi.co/api/v2/pokemon/1/',
+                           weight=69, abilities=['overgrow', 'chlorophyll'], pokemon_type=['grass', 'poison'])
 
         self.assertEqual(expected.__repr__(), data.__repr__())
+
+    def test_pokemon_list_return(self):
+        api = PokeAPI()
+        api.build_pokemon_list(limit=2)
+        data = api.get_pokemon_data()
+
+        self.assertTrue(type(data) == list)
+        
+        for pokemon in data:
+            self.assertTrue(type(pokemon) == Pokemon)
 
 if __name__ == '__main__':
     unittest.main()
